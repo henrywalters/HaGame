2fc6ffc4cc463afcbbf613b584e2f06cd371c759
Henry Walters <henrywalters20@gmail.com>
2020-09-19 17:19:07 -0400

    Test

--- modules/FindSDL2.cmake
+++ modules/FindSDL2.cmake
@@ -34,17 +34,4 @@ if(WIN32)
 
     mark_as_advanced(SDL2_ROOT)
     find_package_handle_standard_args(SDL2 DEFAULT_MSG SDL2_INCLUDE_DIRS SDL2_LIBS SDL2_DLLS)
-else()
-    # On MacOS, should be installed via Macports
-    # On Ubuntu, install with: apt-get install libsdl2-dev
-    find_path(SDL2_INCLUDE_DIRS SDL.h PATH_SUFFIXES SDL2)
-    find_library(_SDL2_LIB SDL2)
-    set(SDL2_LIBS ${SDL2})
-    if(_SDL2_use_main)
-        find_library(_SDL2main_LIB SDL2)
-        list(APPEND SDL2_LIBS ${_SDL2main_LIB})
-    endif()
-
-    mark_as_advanced(SDL2_INCLUDE_DIRS _SDL2_LIB _SDL2main_LIB)
-    find_package_handle_standard_args(SDL2 DEFAULT_MSG SDL2_INCLUDE_DIRS SDL2_LIBS)
 endif()
